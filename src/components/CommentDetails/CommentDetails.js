5829bddf3275ede517ebd536d0a83aab95e0416c
marjia-anee <marjiazaman@gmail.com>
2020-09-06 19:17:54 +0600

    added all components and routing between them

---
+++ src/components/CommentDetails/CommentDetails.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import fakeData from '../../fakeData';
+import ShowImage from '../ShowImage/ShowImage';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 600,
+        border: '2px solid blue',
+        borderRadius: '20px',
+        padding: '20px',
+        margin: '40px',
+    },
+    
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[700],
+        width:80,
+        height:80,
+    },
+}));
+
+const CommentDetails = (props) => {
+    const { name, email, body } = props.comment;
+
+    const classes = useStyles();
+
+    const [image, setImage] = useState([]);
+
+    
+    useEffect(() => {
+        setImage (fakeData)
+    }, [])
+
+    console.log(image);
+
+
+    return (
+            <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar className={classes.avatar}>
+              {
+                    image.map(image => <ShowImage image={image} ></ShowImage>)
+                }
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={name}
+        subheader={email}
+      />
+      
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {body}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+       
+          <ExpandMoreIcon />
+      </CardActions>
+    </Card>
+    );
+};
+
+export default CommentDetails;
---
+++ src/fakeData/socialBuddyImages.js
@@ -0,0 +1,32 @@
+var items = [
+    {
+        
+        image: 'https://bollywoodbingo.files.wordpress.com/2019/03/vicky-kaushal-1.jpg'
+
+    },
+    {
+        
+        image: 'https://res.cloudinary.com/aamong/image/upload/v1517467807/Shah-Rukh-Khan_lsjbbi.jpg'
+
+    },
+    {
+        
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQAmYhwyAskuCygKGeGv9HKJdgmGMvddFw1jQ&usqp=CAU'
+    },
+    {
+        
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQUcNeViOQUAXMDsBC__qR7DmWVistHbwubaw&usqp=CAU'
+
+    },
+    {
+        
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQBjEMnq74Ad0YpIjCXl48t_EW0OOnSBa7DIQ&usqp=CAU'
+
+    },
+
+
+
+    
+];
+
+export default items;
